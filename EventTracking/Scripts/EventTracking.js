33188cae230c7ece90e2858168fe629bfab75af6
Anish Khobragade <anishism753@gmail.com>
2014-09-14 10:21:27 +0530

    event tracking script
    use this script to  fetch data from custom data attributes and  send
    data to google analytics server

--- EventTracking/Scripts/EventTracking.js
+++ EventTracking/Scripts/EventTracking.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
     
     var $t = $('[data-event="event"]');
     $t.click(function () {
-        alert();
+       
         var evCat = $(this).data('category') ? $(this).data('category') : '',
             evAct = $(this).data('action') ? $(this).data('action') : '',
             evLab = $(this).data('label') ? $(this).data('label') : '',
             evVal = $(this).data('value') ? $(this).data('value') : 0;
         try {
             ga('send', 'event', evCat, evAct, evLab, evVal);
-            alert();
+            alert(Event tracking data sent to your google analytics account...remove this alert during implementation from eventracking js file);
         } catch (e) {
             console.log(e);
         }
